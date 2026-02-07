c90df2cdebc528aaa1b0571beb61299fa3f25e1d
Kaviya Srinivasan <kaviyasrini0221@gmail.com>
2026-02-07 10:42:18 +0530

    Time: 4 ms (78.03%) | Memory: 38.4 MB (100.00%) - LeetSync

--- 1708-design-parking-system/design-parking-system.cpp
+++ 1708-design-parking-system/design-parking-system.cpp
@@ -9,33 +9,21 @@ public:
     }
     
     bool addCar(int carType) {
-        if(carType==1){
-            if(big>0){
+        if(carType==1 && big>0){
                 big--;
                 return true;
-            }
-            else{
-                return false;
-            }
-        }
-        if(carType==2){
-            if(medium>0){
-                medium--;
-                return true;
-            }
-            else{
-                return false;
-            }
-        }
-        if(carType==3){
-            if(small>0){
-                small--;
-                return true;
-            }
-            else
-                return false;
         }
 
+        if(carType==2 && medium>0){
+                medium--;
+                return true;
+        }
+
+        if(carType==3 && small>0){
+                small--;
+                return true;
+        }
+        
         return false;
     }
 };
