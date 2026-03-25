ab00f1b56858f2b5e21403e14f4316e6f38bf53d
Kaviya Srinivasan <kaviyasrini0221@gmail.com>
2026-03-25 16:29:33 +0530

    Time: 12 ms (7.86%) | Memory: 48 MB (14.46%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int nonzero = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[nonzero] = nums[i];
+                nonzero++;
+            }
+        }
+
+        for (int i = nonzero; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+
+        System.out.println(Arrays.toString(nums));
+    }
+}
