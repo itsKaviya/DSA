2f776d5528fff0f3ba02f6bbff282a6675691c5b
Kaviya Srinivasan <kaviyasrini0221@gmail.com>
2026-06-23 12:54:20 +0530

    Time: 19 ms (23.84%) | Memory: 36.7 MB (74.26%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
@@ -11,8 +11,7 @@ public:
         }
 
         for(int i=1;i<arr.size();i++){
-            int diff = arr[i] - arr[i-1];
-            if(diff == minDiff){
+            if((arr[i] - arr[i-1]) == minDiff){
                 res.push_back({arr[i-1], arr[i]});
             }
         }
