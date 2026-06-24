8c66f9fa02823606a01a1bce90d263a8087d1457
Kaviya Srinivasan <kaviyasrini0221@gmail.com>
2026-06-24 12:18:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (80.59%) - LeetSync

--- 520-detect-capital/detect-capital.cpp
+++ 520-detect-capital/detect-capital.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
-    bool isUpperCase(char ch){
-        return ch >= 65 && ch <=90;
-    }
-
     bool detectCapitalUse(string word) {
         int uppercount = 0;
         for(int i=0;i<word.size();i++){
-            if(isUpperCase(word[i])){
+            if(isupper(word[i])){
                 uppercount++;
             }
         }
@@ -16,7 +12,7 @@ public:
             return true;
         }else if(uppercount == 0){
             return true;
-        }else if(isUpperCase(word[0]) && uppercount == 1){
+        }else if(isupper(word[0]) && uppercount == 1){
             return true;
         }
         else{
