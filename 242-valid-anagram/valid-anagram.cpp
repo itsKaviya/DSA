c2331f5b5d5c50272241a8f8921d1113e4c8d829
Kaviya Srinivasan <kaviyasrini0221@gmail.com>
2026-06-24 12:57:39 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (25.53%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        vector<int> freq(26,0);
+        for(char ch : s){
+            freq[ch -'a']++;
+        }
+        for(char ch : t){
+            freq[ch -'a']--;
+        }
+
+        for(int i : freq){
+            if(i != 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
