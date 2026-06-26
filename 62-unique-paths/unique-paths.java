fbbde20ed680d50936851472cac193c6c27951f4
Kaviya Srinivasan <kaviyasrini0221@gmail.com>
2026-06-26 12:12:08 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (28.22%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -2,7 +2,7 @@ class Solution {
     public int uniquePaths(int m, int n) {
         // ways to reach the target : arr[m][n] = arr[m][n-1] + arr[m-1][n]
 
-        // memosiation : declaring the number of ways visisting a grid;
+        // Memoizationjk : declaring the number of ways visisting a grid;
         int arr[][] = new int[m][n];
         for(int i= 0;i<m;i++){
             for(int j=0; j<n; j++){
