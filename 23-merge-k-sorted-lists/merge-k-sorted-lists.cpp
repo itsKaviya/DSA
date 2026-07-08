5ee2546c71e1b8e84a6f9decb88c35e2aae596f0
Kaviya Srinivasan <kaviyasrini0221@gmail.com>
2026-07-08 23:49:16 +0530

    Time: 0 ms (100.00%) | Memory: 18.9 MB (23.77%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -13,19 +13,19 @@ class Solution {
 public:
     ListNode* MergeList(ListNode* l1, ListNode* l2){
         ListNode dummy(0);
-        ListNode* tail = &dummy;
+        ListNode* temp = &dummy;
         while(l1 != NULL && l2 != NULL){
             if(l1->val < l2->val){
-                tail->next = l1;
+                temp->next = l1;
                 l1 = l1->next;
             } else {
-                tail->next = l2;
+                temp->next = l2;
                 l2 = l2->next;
             }
-            tail = tail->next;
+            temp = temp->next;
         }
-        if(l1 != NULL) tail->next = l1;
-        if(l2 != NULL) tail->next = l2;
+        if(l1 != NULL) temp->next = l1;
+        if(l2 != NULL) temp->next = l2;
         
         return dummy.next;
     }
