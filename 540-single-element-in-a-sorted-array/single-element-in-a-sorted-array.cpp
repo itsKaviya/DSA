e1d975e252a96e5aff431845070634eb9360156f
Kaviya Srinivasan <kaviyasrini0221@gmail.com>
2026-07-09 00:18:56 +0530

    Time: 27 ms (3.10%) | Memory: 31.7 MB (6.47%) - LeetSync

---
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        map<int,int> freq;
+
+        for(int i=0;i<nums.size();i++){
+            freq[nums[i]]++;
+        }
+
+        for(auto i : freq){
+            if(i.second == 1){
+                return i.first;
+            }
+        }
+
+        return -1;
+    }
+};
