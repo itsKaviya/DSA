b7698f04bbc0012cd739326014b35d766c0e7beb
Kaviya Srinivasan <kaviyasrini0221@gmail.com>
2026-07-13 23:52:54 +0530

    Time: 11 ms (8.72%) | Memory: 12 MB (11.62%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char, int>freq;
+
+        for(char ch : magazine){
+            freq[ch]++;
+        }
+
+        for(char ch : ransomNote){
+            if(freq.find(ch) == freq.end() || freq[ch] <= 0){
+                return false;
+            }
+            freq[ch]--;
+        }
+
+        return true;
+    }
+};
