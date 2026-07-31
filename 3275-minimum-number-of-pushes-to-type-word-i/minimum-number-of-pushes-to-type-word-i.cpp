8e609ba333dda26f0ea4dce4069e433d0f11d103
Kaviya Srinivasan <kaviyasrini0221@gmail.com>
2026-07-31 10:27:21 +0530

    Time: 2 ms (15.78%) | Memory: 8.3 MB (54.24%) - LeetSync

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int count = 0;
+
+        for(int i=0; i<word.length(); i++){
+            count += (i/8)+1;
+        }
+
+        return count;
+    }
+};
